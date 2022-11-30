2fbf829209a4881e46f65ec21ef1182e6110e036
arthurbazz <chibuzorarthur@gmail.com>
2022-11-30 16:38:57 +0100

    export pages

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,17 +1,18 @@
 import React from "react";
-import Homepage from "./pages/homepage";
+// import pages
+import { Hero, About, Projects } from "./pages/homepage";
 // import global sccs file
 import "./styles/global.scss";
 
-function MyButton() {
-  return <button>I'm a button</button>;
-}
+// function MyButton() {
+//   return <button>I'm a button</button>;
+// }
 
 export default function App() {
   return (
     <>
-      <h1>Welcome to my App</h1>
-      <Homepage />
+      {/* <h1>Welcome to my App</h1> */}
+      <Hero />
     </>
   );
 }
