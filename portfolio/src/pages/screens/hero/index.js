f25e3e5cdb742ad04e915a3caaec682af6816f2b
arthurbazz <chibuzorarthur@gmail.com>
2023-01-03 18:10:40 +0100

    update hero section

--- portfolio/src/pages/screens/hero/index.js
+++ portfolio/src/pages/screens/hero/index.js
@@ -1,4 +1,5 @@
 // pages/screens/hero/index.js
+// ********** Hero section **********
 import React from "react";
 
 const Hero = () => {
@@ -12,13 +13,21 @@ const Hero = () => {
         </p> */}
 
         <h1 class="hero-title">
-          Hi, my name is <span class="text-color-main">Chibuzor Arthur Uzoaru</span>
+          Hi, my name is{" "}
+          <span class="text-color-main">Chibuzor Arthur Uzoaru.</span>
           <br />
           I'm a frontend engineer.
         </h1>
         <p>
-
+          A front-end software engineer with expertise in HTML, CSS, JavaScript,
+          ReactJS, Django, and Python.
         </p>
+
+        <button className="btn">
+          <a href="#projects" className="">
+            Check out my work
+          </a>
+        </button>
       </div>
     </section>
   );
