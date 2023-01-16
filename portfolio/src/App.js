897a8753221b065fe12034a505c91d2906c27a63
arthurbazz <chibuzorarthur@gmail.com>
2023-01-16 07:30:03 +0100

    add router

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 // import pages
-import { Hero, About, Projects } from "./pages/homepage";
+import { Hero, About, Projects } from "./components";
 // import global styles
 import "./styles/global.scss";
 
@@ -8,6 +8,7 @@ export default function App() {
   return (
     <>
       <Hero />
+      <Projects />
     </>
   );
 }
