3dbc93277e76c2c79f85a8d66833e3cd2274f1cc
arthurbazz <chibuzorarthur@gmail.com>
2023-01-19 23:09:11 +0100

    add navbar

--- portfolio/src/components/header/header.js
+++ portfolio/src/components/header/header.js
@@ -1,28 +1,64 @@
 // components/header/index.js
+
 import React from "react";
-import { Link } from "react-router-dom";
+import Headroom from "react-headroom";
+// import { StyleContext } from "../../contexts/StyleContext";
+import { greeting, projectsHeader } from "../../portfolio";
 
 const Header = () => {
+  const viewProjects = projectsHeader.display;
+
   return (
-    <header>
-      <div className="container">
-        <div className="header">
-          <div className="logo">
-            <Link to="/">arthurbazz</Link>
-          </div>
-          <nav>
-            <ul>
-              <li>
-                <Link to="/projects">Projects</Link>
-              </li>
-              <li>
-                <Link to="/about">About</Link>
-              </li>
-            </ul>
-          </nav>
-        </div>
-      </div>
-    </header>
+    <Headroom>
+      <header className="navbar">
+        <a href="/" className="logo">
+          <span className="logo-name">{greeting.username}</span>
+        </a>
+        {/* <input className="menu-btn" type="checkbox" id="menu-btn" />
+        <label
+          className="menu-icon"
+          htmlFor="menu-btn"
+          style={{ color: "white" }}
+        >
+          <span className="navicon"></span>
+        </label> */}
+        <ul className="menu">
+          {viewProjects && (
+            <li>
+              <a href="#projects">Projects</a>
+            </li>
+          )}
+          {/* {viewExperience && (
+            <li>
+              <a href="#experience">Work Experiences</a>
+            </li>
+          )}
+          {viewOpenSource && (
+            <li>
+              <a href="#opensource">Open Source</a>
+            </li>
+          )}
+          {viewAchievement && (
+            <li>
+              <a href="#achievements">Achievements</a>
+            </li>
+          )}
+          {viewBlog && (
+            <li>
+              <a href="#blogs">Blogs</a>
+            </li>
+          )}
+          {viewTalks && (
+            <li>
+              <a href="#talks">Talks</a>
+            </li>
+          )} */}
+          <li>
+            <a href="#contact">Contact Me</a>
+          </li>
+        </ul>
+      </header>
+    </Headroom>
   );
 };
 
